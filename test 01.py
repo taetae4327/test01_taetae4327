bc909d4bbbeb8188fafb5f4687c311c4ca760518
taekyu4327 <taekyu4327@gmail.com>
2021-11-08 22:57:53 +0900

    print 연습

---
+++ test 01.py
@@ -0,0 +1,18 @@
+a=1
+b='taekyu'
+print('내 이름은 {}'.format('태규'))
+print(1,b,end='\r')
+time.sleep(1)
+print(2,b,end='\r')
+time.sleep(1)
+print(3,b,end='\r')
+time.sleep(1)
+print(4,b,end='\r')
+time.sleep(1)
+print(5,b,end='\r')
+time.sleep(1)
+print(6,b,end='\r')
+time.sleep(1)
+
+
+       
