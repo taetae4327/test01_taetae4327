5c0f28101340bb8a84fbd876407621a97f904f20
taekyu4327 <taekyu4327@gmail.com>
2021-11-12 15:45:01 +0900

    더하기 계산기 만들고  bool에 대하여 공부

---
+++ 계산기 만들기.py
@@ -0,0 +1,8 @@
+a=int(input('a: '))
+b=int(input('b: '))
+c = a + b
+print('a+b={}'.format(c))
+
+ 
+
+
---
+++ 비교연산 논리연산 (bool).py
@@ -0,0 +1,11 @@
+a=input('a: ')
+b=bool(a)
+print(b)
+
+if b:
+    print('값이 있습니다')
+else :
+    print('값이 없습니다')
+
+
+
