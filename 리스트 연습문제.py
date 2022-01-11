110f262ef521b6d6150d625366d5cc3eda8b7bfc
taekyu4327 <taekyu4327@gmail.com>
2022-01-11 21:27:15 +0900

    no message

---
+++ 리스트 연습문제.py
@@ -0,0 +1,14 @@
+arr1 = [1,2,3,'string']
+print(arr1)
+arr1 += ['a','b','c',1,2,3]
+print('생성한 리스트에 원소 추가:',arr1)
+
+print('string의 위치:',arr1.index('string'))
+print('1의개수:',arr1.count(1))
+print()
+a = "I love you John!"
+print(a)
+a=a.split()
+print('sting의 위치:',arr1.index('string'))
+print('John!의 위치:',a.index('John!'))
+print("a의 길이:",len(a))
