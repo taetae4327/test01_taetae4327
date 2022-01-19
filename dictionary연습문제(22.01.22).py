d91f1fcf5dde993b2b0d7947f1a223d5742ca9a8
taekyu4327 <taekyu4327@gmail.com>
2022-01-19 12:03:50 +0900

    no message

---
+++ dictionary연습문제(22.01.22).py
@@ -0,0 +1,10 @@
+b= {'name':'John',
+    'phone':'01012341234',
+    'email':'test@test.com',
+    'birth':111111}
+print(b)
+print(b['name'])#name data찾기 
+b['birth']=101010#birth data 101010으로 바꾸기 
+print(b['birth'])
+b['city']='Seoul'# city가 key이고 Seoul이 value인 데이터 추가
+print(b)
